91a6a4468ebcf94259f66b363c860cceb4a37359
aoudet <a.oudet@gmail.com>
2022-03-10 11:33:57 -0500

    3 files modified

--- src/app/customs/tpl-autocomplete/tpl-autocomplete.component.ts
+++ src/app/customs/tpl-autocomplete/tpl-autocomplete.component.ts
@@ -1,3 +1,4 @@
+import { FocusMonitor } from '@angular/cdk/a11y';
 import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
 import {
   Component,
@@ -7,9 +8,11 @@ import {
   OnInit,
   Optional,
   Self,
+  ViewChild,
 } from '@angular/core';
 import {
   ControlValueAccessor,
+  FormControl,
   NgControl,
   NG_VALUE_ACCESSOR,
 } from '@angular/forms';
@@ -18,7 +21,7 @@ import {
   MatFormFieldControl,
   MAT_FORM_FIELD,
 } from '@angular/material/form-field';
-import { Observable, Subject } from 'rxjs';
+import { map, Observable, startWith, Subject } from 'rxjs';
 
 @Component({
   selector: 'app-tpl-autocomplete',
@@ -26,15 +29,13 @@ import { Observable, Subject } from 'rxjs';
   styleUrls: ['./tpl-autocomplete.component.scss'],
   providers: [
     { provide: MatFormFieldControl, useExisting: TplAutocompleteComponent },
-    {
-      provide: NG_VALUE_ACCESSOR,
-      multi: true,
-      useExisting: TplAutocompleteComponent,
-    },
   ],
+  host: {
+    '[id]': 'id',
+  },
 })
 export class TplAutocompleteComponent
-  implements MatFormFieldControl<any>, ControlValueAccessor
+  implements MatFormFieldControl<any>, ControlValueAccessor, OnInit
 {
   static nextId = 0;
   stateChanges = new Subject<void>();
@@ -45,11 +46,7 @@ export class TplAutocompleteComponent
   autofilled?: boolean | undefined;
 
   get empty() {
-    // const {
-    //   value: { area, exchange, subscriber },
-    // } = this.parts;
-    return true;
-    // return !area && !exchange && !subscriber;
+    return this.myControl.value === '' || this.myControl.value === null;
   }
 
   get shouldLabelFloat() {
@@ -84,37 +81,37 @@ export class TplAutocompleteComponent
   }
   set disabled(value: BooleanInput) {
     this._disabled = coerceBooleanProperty(value);
-    // this._disabled ? this.parts.disable() : this.parts.enable();
+    this._disabled ? this.myControl.disable() : this.myControl.enable();
     this.stateChanges.next();
   }
   private _disabled = false;
 
   @Input()
   get value(): any | null {
-    // if (this.parts.valid) {
-    //   const {
-    //     value: { area, exchange, subscriber },
-    //   } = this.parts;
-    //   return new MyTel(area, exchange, subscriber);
-    // }
+    if (this.myControl.valid) {
+      return this.myControl.value;
+    }
     return null;
   }
-  set value(tel: any | null) {
-    // const { area, exchange, subscriber } = tel || new MyTel('', '', '');
-    // this.parts.setValue({ area, exchange, subscriber });
+  set value(value: any | null) {
+    this.myControl.setValue(value);
     this.stateChanges.next();
   }
 
   get errorState(): boolean {
-    return false;
-    // return this.parts.invalid && this.touched;
+    return this.myControl.invalid && this.touched;
   }
 
   constructor(
     private _elementRef: ElementRef<HTMLElement>,
+    private _focusMonitor: FocusMonitor,
     @Optional() @Inject(MAT_FORM_FIELD) public _formField: MatFormField,
     @Optional() @Self() public ngControl: NgControl
-  ) {}
+  ) {
+    if (this.ngControl != null) {
+      this.ngControl.valueAccessor = this;
+    }
+  }
   //#region ControlValueAccessor
   onChange = (_: any) => {};
   onTouched = () => {};
@@ -136,14 +133,49 @@ export class TplAutocompleteComponent
     controlElement.setAttribute('aria-describedby', ids.join(' '));
   }
   onContainerClick(event: MouseEvent): void {
-    this.focused = true;
-    // const controlElement =
-    //   this._elementRef.nativeElement.querySelector('input')!;
-    // controlElement.focus();
+    this._focusMonitor.focusVia(this.autoInput, 'program');
   }
 
   ngOnDestroy() {
     this.stateChanges.complete();
-    // this._focusMonitor.stopMonitoring(this._elementRef);
+    this._focusMonitor.stopMonitoring(this._elementRef);
+  }
+
+  @ViewChild('autoInput') autoInput: HTMLInputElement;
+  myControl = new FormControl();
+  options: string[] = ['One', 'Two', 'Three'];
+  filteredOptions: Observable<string[]>;
+
+  ngOnInit() {
+    this.filteredOptions = this.myControl.valueChanges.pipe(
+      startWith(''),
+      map((value) => this._filter(value))
+    );
+  }
+
+  private _filter(value: string): string[] {
+    const filterValue = value.toLowerCase();
+
+    return this.options.filter((option) =>
+      option.toLowerCase().includes(filterValue)
+    );
+  }
+
+  onFocusIn(event: FocusEvent) {
+    if (!this.focused) {
+      this.focused = true;
+      this.stateChanges.next();
+    }
+  }
+
+  onFocusOut(event: FocusEvent) {
+    if (
+      !this._elementRef.nativeElement.contains(event.relatedTarget as Element)
+    ) {
+      this.touched = true;
+      this.focused = false;
+      this.onTouched();
+      this.stateChanges.next();
+    }
   }
 }
