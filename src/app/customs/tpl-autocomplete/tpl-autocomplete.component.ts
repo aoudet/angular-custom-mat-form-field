70af67215e255b0f9344388aee20e3cf34dbe4e8
aoudet <a.oudet@gmail.com>
2022-03-10 11:11:52 -0500

    2 files modified

--- src/app/customs/tpl-autocomplete/tpl-autocomplete.component.ts
+++ src/app/customs/tpl-autocomplete/tpl-autocomplete.component.ts
@@ -1,10 +1,23 @@
-import { Component, ElementRef, OnInit } from '@angular/core';
+import { BooleanInput, coerceBooleanProperty } from '@angular/cdk/coercion';
+import {
+  Component,
+  ElementRef,
+  Inject,
+  Input,
+  OnInit,
+  Optional,
+  Self,
+} from '@angular/core';
 import {
   ControlValueAccessor,
   NgControl,
   NG_VALUE_ACCESSOR,
 } from '@angular/forms';
-import { MatFormFieldControl } from '@angular/material/form-field';
+import {
+  MatFormField,
+  MatFormFieldControl,
+  MAT_FORM_FIELD,
+} from '@angular/material/form-field';
 import { Observable, Subject } from 'rxjs';
 
 @Component({
@@ -23,22 +36,85 @@ import { Observable, Subject } from 'rxjs';
 export class TplAutocompleteComponent
   implements MatFormFieldControl<any>, ControlValueAccessor
 {
-  value: any;
+  static nextId = 0;
   stateChanges = new Subject<void>();
-  id: string;
-  placeholder: string;
-  ngControl: NgControl | null;
-  focused: boolean;
-  empty: boolean;
-  shouldLabelFloat: boolean;
-  required: boolean;
-  disabled: boolean;
-  errorState: boolean;
-  controlType?: string | undefined;
+  id = `tpl-autocomplete-${TplAutocompleteComponent.nextId++}`;
+  focused = false;
+  touched = false;
+  controlType = 'tpl-autocomplete';
   autofilled?: boolean | undefined;
-  userAriaDescribedBy?: string | undefined;
 
-  constructor(private _elementRef: ElementRef<HTMLElement>) {}
+  get empty() {
+    // const {
+    //   value: { area, exchange, subscriber },
+    // } = this.parts;
+    return true;
+    // return !area && !exchange && !subscriber;
+  }
+
+  get shouldLabelFloat() {
+    return this.focused || !this.empty;
+  }
+
+  @Input('aria-describedby') userAriaDescribedBy: string;
+
+  @Input()
+  get placeholder(): string {
+    return this._placeholder;
+  }
+  set placeholder(value: string) {
+    this._placeholder = value;
+    this.stateChanges.next();
+  }
+  private _placeholder: string;
+
+  @Input()
+  get required(): boolean {
+    return this._required;
+  }
+  set required(value: BooleanInput) {
+    this._required = coerceBooleanProperty(value);
+    this.stateChanges.next();
+  }
+  private _required = false;
+
+  @Input()
+  get disabled(): boolean {
+    return this._disabled;
+  }
+  set disabled(value: BooleanInput) {
+    this._disabled = coerceBooleanProperty(value);
+    // this._disabled ? this.parts.disable() : this.parts.enable();
+    this.stateChanges.next();
+  }
+  private _disabled = false;
+
+  @Input()
+  get value(): any | null {
+    // if (this.parts.valid) {
+    //   const {
+    //     value: { area, exchange, subscriber },
+    //   } = this.parts;
+    //   return new MyTel(area, exchange, subscriber);
+    // }
+    return null;
+  }
+  set value(tel: any | null) {
+    // const { area, exchange, subscriber } = tel || new MyTel('', '', '');
+    // this.parts.setValue({ area, exchange, subscriber });
+    this.stateChanges.next();
+  }
+
+  get errorState(): boolean {
+    return false;
+    // return this.parts.invalid && this.touched;
+  }
+
+  constructor(
+    private _elementRef: ElementRef<HTMLElement>,
+    @Optional() @Inject(MAT_FORM_FIELD) public _formField: MatFormField,
+    @Optional() @Self() public ngControl: NgControl
+  ) {}
   //#region ControlValueAccessor
   onChange = (_: any) => {};
   onTouched = () => {};
@@ -61,9 +137,9 @@ export class TplAutocompleteComponent
   }
   onContainerClick(event: MouseEvent): void {
     this.focused = true;
-    const controlElement =
-    this._elementRef.nativeElement.querySelector('input')!;
-    controlElement.focus();
+    // const controlElement =
+    //   this._elementRef.nativeElement.querySelector('input')!;
+    // controlElement.focus();
   }
 
   ngOnDestroy() {
